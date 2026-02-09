867edcf01e17afc8fb53464abd0d0b2f94fe1ec0
Yashwant330 <yashwanthalwai161@gmail.com>
2026-02-09 15:09:33 +0530

    let addes

--- script.js
+++ script.js
@@ -37,7 +37,8 @@ console.log(typeof name); // Output: "string"
 
 
 
-var num1 = 10;
-var num2 = 20;
-var sum = num1 + num2;
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log("The sum of num1 and num2 is: " + sum);
 
