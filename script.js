ced4c33cb5fc74a822ea814627b76f526e799a84
Yashwant330 <yashwanthalwai161@gmail.com>
2026-02-09 15:06:25 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Hello, World!");
+console.warn("This is a simple JavaScript file.");
+console.error("This is an error message.");
+
+
+console.log("Hello, World!");
+alert("Welcome to JavaScript programming!");
+document.write("This is a simple JavaScript file.");
+
+var name = "John";
+let age = 30;
+const PI = 3.14;
+
+
+console.log("Name: " + name);
+console.log("Age: " + age);  
+console.log("Value of PI: " + PI);
+
+
+
+// alert("Welcome to JavaScript programming!");
+prompt("Please enter your name:");
+// confirm("Do you want to continue?");
+
+
+var a=10;
+var b=20;
+var sum=a/b;
+console.log("The sum of a and b is: " + sum);
+
+
+var x = 5;
+var name = "Alice";
+
+console.log(typeof x); // Output: "number"
+console.log(typeof name); // Output: "string"
+
